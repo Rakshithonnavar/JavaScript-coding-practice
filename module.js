acfe51b9e021d1f3d75a166d56465bea6a5bfedd
Rakshit honnavar <rakshithonnavar20@gmail.com>
2024-03-25 21:03:31 +0530

    subdirectory created with files

---
+++ module.js
@@ -0,0 +1,15 @@
+// export const myname = 'RAKSHIT';
+
+//  export function myNAMEDisplay(name)
+// {
+//     console.log(`My name is ${name}`);
+// }
+// console.log(myNAMEDisplay("Rakshit"));
+
+export default function addtwo(a,b)
+{
+     console.log(a+b);
+}
+console.log(addtwo(7,8));
+
+
--- module.mjs
+++
@@ -1,9 +0,0 @@
-export const myname = 'RAKSHIT';
-
- export function myNAMEDisplay(name)
-{
-    console.log(`My name is ${name}`);
-}
-console.log(myNAMEDisplay("Rakshit"));
-
-
