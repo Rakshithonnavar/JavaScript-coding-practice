e795cff7e7b35fc2f3cba33c5f611fb5d21aeca9
Rakshit honnavar <rakshithonnavar20@gmail.com>
2024-03-26 23:37:13 +0530

    minor  changes

--- module.js
+++ module.js
@@ -6,10 +6,19 @@
 // }
 // console.log(myNAMEDisplay("Rakshit"));
 
-export default function addtwo(a,b)
-{
-     console.log(a+b);
-}
-console.log(addtwo(7,8));
+// export default function addtwo(a,b)
+// {
+//      console.log(a+b);
+// }
+// console.log(addtwo(7,8));
 
+function addthree(a,b,c)
+{
+      console.log(a + b + c);
+}
+console.log(addthree(1,2,3));
+
+
+
+// export{addthree};
 
