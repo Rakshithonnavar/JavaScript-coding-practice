f35db556ac1b5f2288ea407249eeac97f75ef768
Rakshit honnavar <rakshithonnavar20@gmail.com>
2024-03-30 13:27:52 +0530

    minor  changes

---
+++ customizeapp/src/components/Sidebar.js
@@ -0,0 +1,19 @@
+// function Sidebar(props)
+// {
+//     return <h2>{props.greet} from Sidebar</h2>;
+// }
+// export default Sidebar;
+
+function jsxlist()
+{
+    return (
+        <ul>
+            <li>{"Raksh"+"hit"}</li>
+            <li>{"Number display"}</li>
+            <li>{2*2}</li>
+            <li>{4}</li>
+            <li></li>
+        </ul>
+    )
+}
+console.log(jsxlist())
