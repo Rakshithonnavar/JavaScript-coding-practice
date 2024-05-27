bfd88597d7c1074098cfdc910b6c05a3eaa48888
Rakshit honnavar <rakshithonnavar20@gmail.com>
2024-05-28 01:25:36 +0530

    latest commit

--- basic-questions.js
+++ basic-questions.js
@@ -745,16 +745,36 @@ const Myprofile = {
 }
 Myprofile.prof();
 
-
-
-
-
-
-
-
-
+function doubleResult(num1,num2,calc)
+{
+  return calc(num1,num2)*2;
+}
 
 // const newSett =  new Set(Rakshit);
+
+function add(num1,num2)
+{
+  return num1+num2;
+}
+
+function multiply(num1,num2)
+{
+   return num1*num2;
+}
+console.log(doubleResult(4,2,add));
+console.log(doubleResult(4,2,multiply));
+
+let global = 'World';
+
+function outer(){
+   let locaglobal = "rakshit";
+  function inner()
+  {
+    let mylocal = `${global}, ${locaglobal}}!`;
+    return mylocal;
+  }
+  return inner;
+}
 
 
 
