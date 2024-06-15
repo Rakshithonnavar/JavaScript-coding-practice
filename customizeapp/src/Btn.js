b8705ac87f3f11c9241966789733bb6283bca567
Rakshithonnavar <rakshithonnavar20@gmail.com>
2024-06-16 00:23:28 +0530

    latest commit

--- customizeapp/src/Btn.js
+++ customizeapp/src/Btn.js
@@ -1,7 +1,21 @@
 function Btn()
 {
+    const clickHandler = ()=>{
+     console.log('clicked'); 
+    }
     return(
         <button onClick ={clickHandler}>Click me</button>
+    );
+}
+export default Btn;
+
+function Btn()
+{
+    const clickHandler = ()=>{
+     console.log('mouse hover'); 
+    }
+    return(
+        <button onMouseOver={clickHandler}>Click me</button>
     );
 }
 export default Btn;
